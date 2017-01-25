ad67f4d47813e1fc8f6a52cc961303589d5f0c0e
alin <alin@wlan141083.mobiel.utwente.nl>
2017-01-25 14:26:43 +0100

    computerplayer

--- src/ss/connect4/ComputerPlayer.java
+++ src/ss/connect4/ComputerPlayer.java
@@ -6,12 +6,12 @@ import java.util.*;
 
 public class ComputerPlayer extends Player {
 
-	private ArrayList<Object> emptyFields;
+	private ArrayList<Object> possibleMoves;
 
 	public static final int DIM = 4;
 	private Board currentBoard;
-	private int smartField;
-	private boolean found;
+	
+	
 
 	public ComputerPlayer(String name, Mark mark) {
 		// create a computer player with the given mark and strategy;
@@ -21,47 +21,18 @@ public class ComputerPlayer extends Player {
 
 	@Override
 	/**
-	 * returns the best playable move according to this AI's ruleset.
+	 * returns a random playable move.
 	 */
 	public int determineMove(Board board) {
-		// TODO Auto-generated method stub
-
+		
 		currentBoard = board.deepCopy();
-		// if(currentBoard.isEmptyField(4) == true) {
-		// smartField = 4;
-		// }
-		// else {
-		emptyFields = new ArrayList<>();
-		for (int i = 0; i < DIM * DIM * DIM; i++) {
-			if (currentBoard.getField(i) == Mark.E) {
-				emptyFields.add(i);
+		possibleMoves = new ArrayList<>();
+			for(int i=0; i < DIM*DIM*DIM; i++) {
+				if(currentBoard.isPlayableField(i)) {
+					possibleMoves.add(i);
+				}	
 			}
-		}
-		found = false;
-		for (int i = 0; i < emptyFields.size() && !found; i++) {
-			currentBoard = board.deepCopy();
-			currentBoard.setField((int) emptyFields.get(i), this.mark);
-			if (currentBoard.hasDiagonal(this.mark) || currentBoard.hasRow(this.mark)) {
-				smartField = (int) emptyFields.get(i);
-				found = true;
-			} else {
-				currentBoard.setField((int) emptyFields.get(i), this.mark.other());
-				if (currentBoard.hasDiagonal(this.mark.other()) || currentBoard.hasRow(this.mark.other())) {
-					smartField = (int) emptyFields.get(i);
-					found = true;
-				}
-			}
-		}
-		// if(!found){
-		// look for 1 or 2 on the same row, diagonal or column
-
-		// }
-
-		if (!found) {
-			smartField = (int) (Math.random() * emptyFields.size() + 1);
-
-		}
-		return smartField;
+	    return (int) (Math.random() * possibleMoves.size() + 1);
 
 	}
 }
--- src/ss/connect4/ComputerPlayerAdv.java
+++ src/ss/connect4/ComputerPlayerAdv.java
@@ -10,7 +10,7 @@ import ss.connect4.Mark;
 public class ComputerPlayerAdv extends Player {
 
 	
-	private ArrayList<Object> emptyFields;
+	private ArrayList<Object> possibleMoves;
 	
     public static final int DIM = 4;
 	private Board currentBoard;
@@ -21,63 +21,47 @@ public class ComputerPlayerAdv extends Player {
 	public ComputerPlayerAdv(String name, Mark mark) {
 		 //create a computer player with the given mark and strategy;
 		super(name, mark);
-		
-	   
-		
 	}
 	
-
-
 	@Override
 	/**
 	 * returns the best playable move according to this AI's ruleset.
 	 */
 	public int determineMove(Board board) {
-		// TODO Auto-generated method stub
-		
-		currentBoard = board.deepCopy();
-//		if(currentBoard.isEmptyField(4) == true) {
-//			smartField = 4;
-//		}
-//		else {
-			emptyFields = new ArrayList<>();
-			for(int i=0; i < DIM*DIM*DIM; i++) {
-				if(currentBoard.getField(i) == Mark.E) {
-					emptyFields.add(i);
-				}	
-			}
-			found = false;
-			for(int i=0; i < emptyFields.size() && !found; i++) {
-				currentBoard = board.deepCopy();
-				currentBoard.setField((int)emptyFields.get(i), this.mark);
-				if(currentBoard.hasDiagonal(this.mark) || currentBoard.hasRow(this.mark)) {
-					smartField = (int) emptyFields.get(i);
-					found = true;
-				}
-				else {
-				    currentBoard.setField((int)emptyFields.get(i), this.mark.other());
-				    if(currentBoard.hasDiagonal(this.mark.other()) || currentBoard.hasRow(this.mark.other())) {
-					    smartField = (int) emptyFields.get(i); 
-					    found = true;
-				    }
-				}
-			}
-//			if(!found){
-			// look for 1 or 2 on the same row, diagonal or column
-			
-			
-		
-	//	}
-		
-		if(!found) {
-		smartField = (int)(Math.random() * emptyFields.size() + 1);
-		
-	    }
-	return smartField;
 	
 		
+		currentBoard = board.deepCopy();
+		possibleMoves = new ArrayList<>();
+			for(int i=0; i < DIM*DIM*DIM; i++) {
+				if(currentBoard.isPlayableField(i)) {
+					possibleMoves.add(i);
+				}	
+			}
+			
+		found = false;
+		for(int i=0; i < possibleMoves.size() && !found; i++) {
+			currentBoard = board.deepCopy();
+			currentBoard.setField((int)possibleMoves.get(i), this.mark);
+			if(currentBoard.hasDiagonal(this.mark) || currentBoard.hasRow(this.mark)) {
+				smartField = (int) possibleMoves.get(i);
+				found = true;
+			}
+			else {
+				currentBoard.setField((int)possibleMoves.get(i), this.mark.other());
+				if(currentBoard.hasDiagonal(this.mark.other()) || currentBoard.hasRow(this.mark.other())) {
+					smartField = (int) possibleMoves.get(i); 
+					found = true;
+				}
+			}
+		}
+
+			// look for 1 or 2 on the same row, diagonal or column
+			
+		if(!found) {
+		smartField = (int)(Math.random() * possibleMoves.size() + 1);
 		
-		
+	    }
+	    return smartField;
 	
 	}
 }
